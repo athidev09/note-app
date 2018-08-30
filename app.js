6deae9bf0629f29ba8f14fb903f0519eeb21ba61
athidev09 <athiradas09@gmail.com>
2018-08-30 15:15:39 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log('Starting app');
+ const fs = require('fs');
+ const _ = require('lodash');
+
+ //console.log(_.isString(true));
+// console.log(_.isString('Verint'));
+
+
+var filteredArray = _.uniq([1,2,3,3,3,1,1,2,'Apple',1,2,3,'Apple']);
+console.log(filteredArray);
+
+
+
+
+
+
+
+
+
+
+//const notes = require('./notes');
+//console.log('Result',notes.div(9,3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const os = require('os');
+//var user = os.userInfo();
+//console.log(user);
+//console.log(notes);
+//fs.appendFile('greetings.txt','Hello ' + user.username+'!',(err)=>
+//  if (err) throw err;
+  //console.log('The "data to append" was appended to file!');
+//})
---
+++ notes.js
@@ -0,0 +1,13 @@
+console.log("notes file printing!!");
+//module.exports.age=33;
+module.exports.addNote = () => {
+  console.log("add note");
+  return "new note";
+};
+module.exports.add = (a,b) => {
+  return a + b;
+};
+
+module.exports.div = (a,b) => {
+  return a / b;
+};
